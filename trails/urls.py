847c865d6fd5aaa1c26f4e747c35946e4bb084b9
njogubless <pnjogubless@gmail.com>
2025-07-16 16:12:25 +0300

    defining urls for the trails app

--- trails/urls.py
+++ trails/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from . import views
+from .views import (
+    TrailListCreateView,
+    TrailDetailView,
+    NearbyTrailsView,
+    TrailImageUploadView,
+    TrailReviewListCreateView,
+    TrailConditionListCreateView
+)
 
-urlpatterns =[
-    path("", views.home, name="home"),
-    
+urlpatterns = [
+    path('', TrailListCreateView.as_view(), name='trail-list-create'),
+    path('<int:pk>/', TrailDetailView.as_view(), name='trail-detail'),
+    path('nearby/', NearbyTrailsView.as_view(), name='nearby-trails'),
+    path('images/upload/', TrailImageUploadView.as_view(), name='trail-image-upload'),
+    path('<int:trail_id>/reviews/', TrailReviewListCreateView.as_view(), name='trail-reviews'),
+    path('<int:trail_id>/conditions/', TrailConditionListCreateView.as_view(), name='trail-conditions'),
 ]
